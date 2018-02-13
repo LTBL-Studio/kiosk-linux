099df415d57f903fbd6c989f2db230ad3b76c7e7
EpicKiwi <me@epickiwi.fr>
2018-02-13 16:17:49 +0100

    service de clear

---
+++ install-kiosk/kuser-files/clean-update.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+cd /home/kiosk
+echo "Nettoyage des précédentes mise à jour"
+if [ -f ./update.lock ]; then
+	echo "Une ancienne Mise à jour etait en cours sur cette machine."
+	echo "L'application sera peut être instable."
+	rm ./update.lock
+fi
