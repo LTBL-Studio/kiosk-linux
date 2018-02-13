7bafb39196070f41103245f1769c3f1b217955cf
EpicKiwi <me@epickiwi.fr>
2018-02-13 16:51:02 +0100

    demarrage avec le système

--- install-kiosk/kuser-files/clean-update.sh
+++ install-kiosk/kuser-files/clean-update.sh
@@ -5,4 +5,5 @@ if [ -f ./update.lock ]; then
 	echo "Une ancienne Mise à jour etait en cours sur cette machine."
 	echo "L'application sera peut être instable."
 	rm ./update.lock
+	echo "cleaned" >> ./app.version
 fi
